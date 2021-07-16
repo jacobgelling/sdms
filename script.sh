19e06df16137aef2d98292d0b46cda71963074e7
Jacob Gelling <jacobgelling@users.noreply.github.com>
2021-07-16 15:55:56 +0200

    Fix PHP check

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ sdms_www="/srv/www"
 sdms_php="7.0"
 if [ -e "/etc/php/7.3/fpm/php.ini" ] && [ -e "/etc/php/7.3/cli/php.ini" ]; then
     sdms_php="7.3"
-elif  -e "/etc/php/7.4/fpm/php.ini" ] && [ -e "/etc/php/7.4/cli/php.ini" ]; then
+elif [ -e "/etc/php/7.4/fpm/php.ini" ] && [ -e "/etc/php/7.4/cli/php.ini" ]; then
     sdms_php="7.4"
 fi
 
@@ -60,7 +60,7 @@ sdms_deploy() {
     # Update PHP version
     if [ -e "/etc/php/7.3/fpm/php.ini" ] && [ -e "/etc/php/7.3/cli/php.ini" ]; then
         sdms_php="7.3"
-    elif  -e "/etc/php/7.4/fpm/php.ini" ] && [ -e "/etc/php/7.4/cli/php.ini" ]; then
+    elif [ -e "/etc/php/7.4/fpm/php.ini" ] && [ -e "/etc/php/7.4/cli/php.ini" ]; then
         sdms_php="7.4"
     fi
 
