f7bb73ddf25224454d3e397dd928f059140a29d1
Jacob Gelling <jacobgelling@users.noreply.github.com>
2023-07-07 11:28:26 +0200

    More robust downloading

--- cloudflare.sh
+++ cloudflare.sh
@@ -2,20 +2,26 @@
 # MIT License
 # Copyright (c) 2019 Jacob Gelling
 
+# Set NGINX real IP header
+echo "real_ip_header CF-Connecting-IP;" > /etc/nginx/snippets/cloudflare.conf
+
 # Get Cloudflare IPv4 address ranges
-curl -s https://www.cloudflare.com/ips-v4 > /etc/nginx/snippets/cloudflare.conf
-echo "" >> /etc/nginx/snippets/cloudflare.conf
+curl_result=$(curl -s https://www.cloudflare.com/ips-v4)
+if [ $? -eq 0 ]; then
+    echo "$curl_result" | sed 's/^/set_real_ip_from /; s/$/;/' >> /etc/nginx/snippets/cloudflare.conf
+else
+    echo "sdms failed to download Cloudflare IPv4 address ranges"
+    exit 1
+fi
 
 # Get Cloudflare IPv6 address ranges
-curl -s https://www.cloudflare.com/ips-v6 >> /etc/nginx/snippets/cloudflare.conf
-echo "" >> /etc/nginx/snippets/cloudflare.conf
-
-# Set NGINX real IP addresses
-sed -i 's/^/set_real_ip_from /' /etc/nginx/snippets/cloudflare.conf
-sed -i 's/$/;/' /etc/nginx/snippets/cloudflare.conf
-
-# Set NGINX real IP header
-echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/snippets/cloudflare.conf
+curl_result=$(curl -s https://www.cloudflare.com/ips-v6)
+if [ $? -eq 0 ]; then
+    echo "$curl_result" | sed 's/^/set_real_ip_from /; s/$/;/' >> /etc/nginx/snippets/cloudflare.conf
+else
+    echo "sdms failed to download Cloudflare IPv6 address ranges"
+    exit 1
+fi
 
 # Include cloudflare.conf in nginx.conf within http block
 if ! grep -q "include snippets/cloudflare.conf;" /etc/nginx/nginx.conf; then
